fd18647b91b63ca32f84d5049ec0c19c4db1df24
csing95 <csinger5195@gmail.com>
2019-03-06 12:17:47 -0600

    1.2 Updated listview

--- app/src/main/java/edu/txstate/cas388/cityguide/MainActivity.java
+++ app/src/main/java/edu/txstate/cas388/cityguide/MainActivity.java
@@ -1,9 +1,14 @@
 package edu.txstate.cas388.cityguide;
 
 import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends ListActivity {
     //eventually in database
@@ -14,8 +19,28 @@ public class MainActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main); //must comment out this line or runtime error will occur
 
-        //Define the data type of the elements in the array (data source, model
+//        Define the data type of the elements in the array (data source, model
+//        setListAdapter(new ArrayAdapter<String>(MainActivity.this,
+//                android.R.layout.simple_list_item_1, strAttractions));
         setListAdapter(new ArrayAdapter<String>(MainActivity.this,
-                android.R.layout.simple_list_item_1, strAttractions));
+                R.layout.activity_main, R.id.txtTravel, strAttractions));
+
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+//        position = index (position in array)
+//        super.onListItemClick(l, v, position, id);
+
+
+        /*if (position == 0){
+            Toast.makeText(MainActivity.this, "You selected " + strAttractions[0],Toast.LENGTH_LONG).show();
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://artic.edu")));
+        }*/
+
+        //look up selected element in array
+        String strIndex =strAttractions[position];
+        Toast.makeText(MainActivity.this, "You selected " + strIndex + ".",Toast.LENGTH_LONG).show();
+
     }
 }
