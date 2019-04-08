df2aa9d6e6d4151cced81be5765231c3692ed595
csing95 <csinger5195@gmail.com>
2019-04-08 16:08:57 -0500

    1.6

--- app/src/main/java/edu/txstate/cas388/cityguide/AttractionInfoActivity.java
+++ app/src/main/java/edu/txstate/cas388/cityguide/AttractionInfoActivity.java
@@ -4,6 +4,12 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
 
 public class AttractionInfoActivity extends AppCompatActivity {
 
@@ -23,6 +29,35 @@ public class AttractionInfoActivity extends AppCompatActivity {
         dblCost = sharedPref2.getInt("cost", 0);
         strURL = sharedPref2.getString("url", "");
         strName = sharedPref2.getString("name", "");
+
+        Button btnCostCalculation = findViewById(R.id.btnCalculateCostAttr);
+        final Button btnGoToWebsite = findViewById(R.id.btnGoToWebsite);
+        final EditText txtNumberOfPersons = findViewById(R.id.txtNumberOfPersons);
+
+        if (strURL.equals("")){
+            btnGoToWebsite.setEnabled(false);
+        }
+
+        btnGoToWebsite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (btnGoToWebsite.isEnabled()){
+                    //start an activity
+                }
+            }
+        });
+
+
+        btnCostCalculation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int intPersons = Integer.parseInt(txtNumberOfPersons.getText().toString());
+                double dblTotalCost = dblCost * intPersons;
+                DecimalFormat currency = new DecimalFormat("$###,###.00");
+                Toast.makeText(AttractionInfoActivity.this, "Total Cost: " + currency.format(dblTotalCost), Toast.LENGTH_LONG).show();
+
+            }
+        });
 
     }
 }
