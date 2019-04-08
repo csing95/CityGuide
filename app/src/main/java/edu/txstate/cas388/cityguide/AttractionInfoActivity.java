3013636e713d15ac43a8afbf17941a4288575841
csing95 <csinger5195@gmail.com>
2019-04-08 16:12:01 -0500

    1.6

--- app/src/main/java/edu/txstate/cas388/cityguide/AttractionInfoActivity.java
+++ app/src/main/java/edu/txstate/cas388/cityguide/AttractionInfoActivity.java
@@ -12,52 +12,55 @@ import android.widget.Toast;
 import java.text.DecimalFormat;
 
 public class AttractionInfoActivity extends AppCompatActivity {
-
     int intId;
     double dblCost;
-    String strURL;
-    String strName;
-
+    String strUrl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attraction_info);
 
         SharedPreferences sharedPref2 = PreferenceManager.getDefaultSharedPreferences(this);
-
         intId = sharedPref2.getInt("id", 0);
-        dblCost = sharedPref2.getInt("cost", 0);
-        strURL = sharedPref2.getString("url", "");
-        strName = sharedPref2.getString("name", "");
+        dblCost = sharedPref2.getFloat("cost", 0);
+        strUrl = sharedPref2.getString("url", "");
 
-        Button btnCostCalculation = findViewById(R.id.btnCalculateCostAttr);
-        final Button btnGoToWebsite = findViewById(R.id.btnGoToWebsite);
-        final EditText txtNumberOfPersons = findViewById(R.id.txtNumberOfPersons);
+        Button costCalculation = findViewById(R.id.btnCalculateCostAttr);
+        final Button goToWebsite = findViewById(R.id.btnGoToWebsite);
 
-        if (strURL.equals("")){
-            btnGoToWebsite.setEnabled(false);
+        if (strUrl.equals("")){
+            goToWebsite.setEnabled(false);
         }
 
-        btnGoToWebsite.setOnClickListener(new View.OnClickListener() {
+        goToWebsite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (btnGoToWebsite.isEnabled()){
-                    //start an activity
+                if (goToWebsite.isEnabled()) {
+                    //Start an activity
                 }
+
             }
         });
 
 
-        btnCostCalculation.setOnClickListener(new View.OnClickListener() {
+
+        final EditText numberOfPersons = findViewById(R.id.txtNumberOfPersons);
+
+        costCalculation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int intPersons = Integer.parseInt(txtNumberOfPersons.getText().toString());
+                int intPersons = Integer.parseInt(numberOfPersons.getText().toString());
                 double dblTotalCost = dblCost * intPersons;
-                DecimalFormat currency = new DecimalFormat("$###,###.00");
-                Toast.makeText(AttractionInfoActivity.this, "Total Cost: " + currency.format(dblTotalCost), Toast.LENGTH_LONG).show();
+                DecimalFormat currency = new DecimalFormat("$###,###.##");
+                Toast.makeText(AttractionInfoActivity.this, "Total Cost:" +
+                        currency.format(dblTotalCost) + ".", Toast.LENGTH_LONG).show();
 
             }
         });
+
+
+
+
 
     }
 }
